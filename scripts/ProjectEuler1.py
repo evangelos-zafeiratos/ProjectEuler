77e70ebde9345424988ed718184c30c8adf9f81c
Evangelos Zafeiratos <74437675+evangelos-zafeiratos@users.noreply.github.com>
2021-12-20 22:07:50 +0200

    Update ProjectEuler1.py

--- scripts/ProjectEuler1.py
+++ scripts/ProjectEuler1.py
@@ -1,7 +1,11 @@
-sum = 0
-i = 0
-while i<1000:
-    if (i%3 == 0) or (i%5 == 0):
-        sum = sum + i
-    i=i+1
-print(sum)
+# multiples of 3 and 5 below 1000
+
+if __name__ == "__main__":
+
+    sum = 0
+    i = 0
+    while i<1000:
+        if (i%3 == 0) or (i%5 == 0):
+            sum = sum + i
+        i=i+1
+    print(sum)
