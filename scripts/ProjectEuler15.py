247a1ec38e1704148fbb46f7956532d9bf74188c
Evangelos Zafeiratos <74437675+evangelos-zafeiratos@users.noreply.github.com>
2021-12-21 22:30:51 +0200

    Update ProjectEuler15.py

--- scripts/ProjectEuler15.py
+++ scripts/ProjectEuler15.py
@@ -1,18 +1,22 @@
-lattice_path_length = 20
-rows = lattice_path_length
-columns = lattice_path_length
+# Calculate lattice paths for a 20x20 grid
 
-lattice_path = [[0 for x in range(rows)] for x in range(columns)]
+if __name__ == "__main__":
 
-lattice_path[0][0] = 2
-for i in range(1,rows):
-    for j in range(0,i+1):
-        if j == i:
-            lattice_path[i][j] = sum(lattice_path[i-1]) * 2
-        else:
-            k = j
-            while k<i and k>=0:
-                lattice_path[i][j] = lattice_path[i][j] + lattice_path[i-1][k]
-                k = k - 1
+    lattice_path_length = 20
+    rows = lattice_path_length
+    columns = lattice_path_length
 
-print(sum(lattice_path[lattice_path_length-1]))
+    lattice_path = [[0 for x in range(rows)] for x in range(columns)]
+
+    lattice_path[0][0] = 2
+    for i in range(1,rows):
+        for j in range(0,i+1):
+            if j == i:
+                lattice_path[i][j] = sum(lattice_path[i-1]) * 2
+            else:
+                k = j
+                while k<i and k>=0:
+                    lattice_path[i][j] = lattice_path[i][j] + lattice_path[i-1][k]
+                    k = k - 1
+
+    print(sum(lattice_path[lattice_path_length-1]))
