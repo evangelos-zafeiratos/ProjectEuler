06b35579e97a7ea61774db5d9855340e20215104
Evangelos Zafeiratos <74437675+evangelos-zafeiratos@users.noreply.github.com>
2021-12-22 22:45:39 +0200

    Update ProjectEuler10.py

--- scripts/ProjectEuler10.py
+++ scripts/ProjectEuler10.py
@@ -1,21 +1,24 @@
 #Function to identify if number is prime
-def is_prime(x):
-    for i in range (2, x//2 + 1):
-        if x % i == 0:
-            return False
-            break
-        else:
-            continue
-    return True
 
-prime_list = list()
-first_prime = 2
-prime_list.append(first_prime)
-number = 3
+if __name__ == "__main__"":
 
-while (number < 2000000) :
-    if is_prime(number):
-        prime_list.append(number)
-    number = number + 2
+    def is_prime(x):
+        for i in range (2, x//2 + 1):
+            if x % i == 0:
+                return False
+                break
+            else:
+                continue
+        return True
 
-print(sum(prime_list))
+    prime_list = list()
+    first_prime = 2
+    prime_list.append(first_prime)
+    number = 3
+
+    while (number < 2000000) :
+        if is_prime(number):
+            prime_list.append(number)
+        number = number + 2
+
+    print(sum(prime_list))
