1985ef235d5738eac112f5cd3f60c26798ca66d7
Evangelos Zafeiratos <74437675+evangelos-zafeiratos@users.noreply.github.com>
2021-12-23 20:16:40 +0200

    Update ProjectEuler20.py

--- scripts/ProjectEuler20.py
+++ scripts/ProjectEuler20.py
@@ -1,12 +1,16 @@
-def factorial(n):
-    if n == 1:
-        result = 1
-    else:
-        result = n*factorial(n-1)
-    return result
+# Calculate the sum of the digits in the number 100!
 
-sum = 0
-fact_100 = str(factorial(100))
-for digit in fact_100:
-    sum += int(digit)
-print(sum)
+if __name__ == '__main__':
+    
+    def factorial(n):
+        if n == 1:
+            result = 1
+        else:
+            result = n*factorial(n-1)
+        return result
+
+    sum = 0
+    fact_100 = str(factorial(100))
+    for digit in fact_100:
+        sum += int(digit)
+    print(sum)
