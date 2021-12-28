e8ec98b17afddd4d381c7a7228313895e8f24ba4
Evangelos Zafeiratos <74437675+evangelos-zafeiratos@users.noreply.github.com>
2021-12-28 18:36:15 +0200

    Update ProjectEuler22.py

--- scripts/ProjectEuler22.py
+++ scripts/ProjectEuler22.py
@@ -1,14 +1,17 @@
-def calculate_value(name):
+if __name__ == "__main__":
+    
+    def calculate_value(name):
     score = 0
     for char in name:
         score += ord(char) - 64
     return(score)
-
-total_score = 0
-with open(r'C:\Users\Vangelis\Desktop\Data Science Path\Project Euler\p022_names.txt') as f:
-    names_file = f.read().replace('"','').split(",")
-    names_file.sort()
-    for i in range (0,len(names_file),1):
-        total_score = total_score + calculate_value(names_file[i])*(i+1)
+    
+    total_score = 0
+    with open(r'C:\Users\Vangelis\Desktop\Data Science Path\Project Euler\p022_names.txt') as f:
+        names_file = f.read().replace('"','').split(",")
+        names_file.sort()
+        for i in range (0,len(names_file),1):
+            total_score = total_score + calculate_value(names_file[i])*(i+1)
 
     print(total_score)
+
