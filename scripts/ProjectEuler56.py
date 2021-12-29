09e0ecfcf371cd24b409e17cc814d74e6ef43fb4
Evangelos Zafeiratos <74437675+evangelos-zafeiratos@users.noreply.github.com>
2021-12-29 17:52:39 +0200

    Update ProjectEuler56.py

--- scripts/ProjectEuler56.py
+++ scripts/ProjectEuler56.py
@@ -1,15 +1,17 @@
-max = 0
-max_a = 0
-max_b = 0
+if __name__ == '__main__':
 
-for a in range(1,100,1):
-    for b in range(1,100,1):
-        number = a**b
-        str_number = str(number)
-        digits_sum = 0
-        for char in str_number:
-            digits_sum += int(char)
-        if digits_sum > max:
-            max, max_a, max_b = digits_sum, a, b
+    max = 0
+    max_a = 0
+    max_b = 0
 
-print(max,max_a,max_b)
+    for a in range(1,100,1):
+        for b in range(1,100,1):
+            number = a**b
+            str_number = str(number)
+            digits_sum = 0
+            for char in str_number:
+                digits_sum += int(char)
+            if digits_sum > max:
+                max, max_a, max_b = digits_sum, a, b
+
+    print(max,max_a,max_b)
